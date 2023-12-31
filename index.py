70fca952c414893bcf62ea58276e52fc3f44f1fd
wobYY <woby1337@gmail.com>
2023-12-31 23:35:50 +0100

    Drawing the outline for the profile

--- index.py
+++ index.py
@@ -8,12 +8,40 @@ import pandas as pd
 # Docs: https://wiki.freecad.org/Embedding_FreeCAD
 freecad_path = "/usr/lib/freecad-python3/lib"
 sys.path.append(freecad_path)
-import FreeCAD # Import FreeCAD after adding the path
+import FreeCAD  # Import FreeCAD after adding the path
+
 
 # Function to draw the profile from the csv
 def draw_from_csv_coordinates(coordinates, **kwargs):
-    
+    # To activate Part Design workbench:
+    # Gui.activateWorkbench("PartDesignWorkbench")
+
+    # To create a new document
+    # App.newDocument()
+
+    # Rename the newly created document
+    # FreeCAD.getDocument('Unnamed').Label = <new_file_name>
+
+    # Add a new body to the document
+    # App.activeDocument().addObject('PartDesign::Body','Body')
+
+    # Rename the body
+    # App.getDocument('Unnamed').getObject('Body').Label = <new_body_name>
+    # App.getDocument('Unnamed').recompute()
+
+    # For the Body add a sketch in teh YZ plane
+    # App.getDocument('Unnamed').getObject('Body').newObject('Sketcher::SketchObject','Sketch')
+    # App.getDocument('Unnamed').getObject('Sketch').Support = (App.getDocument('Unnamed').getObject('YZ_Plane'),[''])
+    # App.getDocument('Unnamed').getObject('Sketch').MapMode = 'FlatFace'
+    # App.getDocument('Unnamed').recompute()
+    # import PartDesignGui
+
+    # Create Points for each coordinate
+    # App.getDocument('Unnamed').getObject('Sketch').addGeometry(Part.Point(App.Vector(x,y,0)))
+    # RECOMPUTE AFTER YOU CREATE A POINT: App.getDocument('Unnamed').recompute()
+    #
     return
+
 
 # Get the current working directory
 cwd = os.getcwd()
