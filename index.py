d6959049693c6816ae1e1071c24dff782cea924a
wobY <roberto.koren@grid.gg>
2024-01-06 21:11:37 +0100

    Comments, scaling, and data validation

--- index.py
+++ index.py
@@ -16,16 +16,49 @@ import FreeCAD  # Import FreeCAD after adding the path
 import FreeCADGui  # Import FreeCADGui after adding the path
 from FreeCAD import Base
 
-# Because PartDesign is not imported by default
+# Because PartDesign is not imported by default with the imports above
 # We need to import the module folder for PartDesign
 # Forum: https://forum.freecad.org/viewtopic.php?style=4&p=677043#p677043
 sys.path.append("/usr/lib/freecad/Mod")
-import PartDesign  # Import PartDesign after adding the path
+import PartDesign  # Import PartDesign after adding the Mod path
 
 
 # Function to draw the profile from the csv
 def draw_from_csv_coordinates(name, coordinates, **kwargs):
+    # Make sure that the "coordinates" variable is a dataframe
+    if not isinstance(coordinates, pd.DataFrame):
+        log.error("The coordinates variable is not a dataframe")
+        raise TypeError("The coordinates variable is not a dataframe")
+
+    # Check if the dataframe has the correct columns
+    if not "x" in coordinates.columns or not "y" in coordinates.columns:
+        log.error("The dataframe doesn't have the correct columns")
+        raise ValueError("The dataframe doesn't have the correct columns")
+
+    # Check the type of values in the dataframe and convert them to floats
+    log.debug("Checking the type of values in the dataframe")
+    if coordinates["x"].dtype != float:
+        log.debug("Converting the x values to floats")
+        coordinates["x"] = coordinates["x"].astype(float)
+    if coordinates["y"].dtype != float:
+        log.debug("Converting the y values to floats")
+        coordinates["y"] = coordinates["y"].astype(float)
+
+    # Ask the user if they want to scale the profile
+    # If they do, ask them for the scale factor
+    # If they don't, set the scale factor to 1
+    log.debug("Checking if the user wants to scale the profile")
+    scale_factor = 1
+    if input(f"Do you want to scale the {name} profile? (y/n): ").lower() == "y":
+        log.debug("User wants to scale the profile")
+        scale_factor = float(input("Enter the scale factor: "))
+        log.debug("Scale factor set to: %s", scale_factor)
+        coordinates["x"] = coordinates["x"] * scale_factor
+        coordinates["y"] = coordinates["y"] * scale_factor
+        log.debug("Profile scaled")
+
     # Check if "cad" folder exists, if it doesn't create it
+    log.debug("Checking if cad folder exists")
     if not os.path.exists(f"{cwd}/cad"):
         log.debug("cad folder doesn't exist, creating it")
         os.mkdir(f"{cwd}/cad")
